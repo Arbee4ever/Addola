88b96a1609b8f49aa9ea6a88d22e9e1eb0568dc8
Arbee4ever <arbee@arbeeco.de>
2022-03-21 14:16:58 +0100

    fixed villagersFollow gamerule

---
+++ src/main/java/net/arbee/addola/entity/ai/goal/EmeraldGoal.java
@@ -0,0 +1,35 @@
+package net.arbee.addola.entity.ai.goal;
+
+import net.arbee.addola.Gamerules;
+import net.minecraft.entity.ai.TargetPredicate;
+import net.minecraft.entity.ai.goal.TemptGoal;
+import net.minecraft.entity.mob.PathAwareEntity;
+import net.minecraft.recipe.Ingredient;
+
+public class EmeraldGoal extends TemptGoal {
+    private static final TargetPredicate TEMPTING_ENTITY_PREDICATE = (new TargetPredicate()).setBaseMaxDistance(10.0D).includeInvulnerable().includeTeammates().ignoreEntityTargetRules().includeHidden();
+    private int cooldown;
+
+    public EmeraldGoal(PathAwareEntity mob, double speed, Ingredient food, boolean canBeScared) {
+        super(mob, speed, food, canBeScared);
+    }
+
+    @Override
+    public boolean canStart() {
+        if (this.mob.world.getGameRules().getBoolean(Gamerules.VILLAGERS_FOLLOW)) {
+            if (this.cooldown > 0) {
+                --this.cooldown;
+                return false;
+            } else {
+                this.closestPlayer = this.mob.world.getClosestPlayer(TEMPTING_ENTITY_PREDICATE, this.mob);
+                if (this.closestPlayer == null) {
+                    return false;
+                } else {
+                    return this.isTemptedBy(this.closestPlayer.getMainHandStack()) || this.isTemptedBy(this.closestPlayer.getOffHandStack());
+                }
+            }
+        } else {
+            return false;
+        }
+    }
+}
--- src/main/java/net/arbee/addola/mixins/VillagerMixin.java
+++ src/main/java/net/arbee/addola/mixins/VillagerMixin.java
@@ -1,8 +1,8 @@
 package net.arbee.addola.mixins;
 
-import net.arbee.addola.Gamerules;
+import net.arbee.addola.entity.ai.goal.EmeraldGoal;
 import net.minecraft.entity.EntityType;
-import net.minecraft.entity.ai.goal.TemptGoal;
+import net.minecraft.entity.ai.goal.Goal;
 import net.minecraft.entity.passive.MerchantEntity;
 import net.minecraft.entity.passive.VillagerEntity;
 import net.minecraft.item.Items;
@@ -15,19 +15,16 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(VillagerEntity.class)
 public abstract class VillagerMixin extends MerchantEntity {
-
 	public VillagerMixin(EntityType<? extends MerchantEntity> entityType, World world) {
 		super(entityType, world);
 	}
 
 	@Inject(at = @At("HEAD"), method = "mobTick")
 	protected void mobTick(CallbackInfo ci) {
-		if(getEntityWorld().getGameRules().getBoolean(Gamerules.VILLAGERS_FOLLOW)) {
-			goalSelector.add(2, new TemptGoal(this, .8D, false, Ingredient.ofItems(Items.EMERALD_BLOCK, Items.EMERALD_ORE)));
-			goalSelector.add(2, new TemptGoal(this, .4D, true, Ingredient.ofItems(Items.EMERALD)));
-		} else {
-			goalSelector.remove(new TemptGoal(this, .8D, false, Ingredient.ofItems(Items.EMERALD_BLOCK)));
-			goalSelector.remove(new TemptGoal(this, .4D, true, Ingredient.ofItems(Items.EMERALD, Items.EMERALD_ORE)));
-		}
+		final Goal EmeraldBlockGoal = new EmeraldGoal(this, .8D, Ingredient.ofItems(Items.EMERALD_BLOCK), false);
+		final Goal EmeraldOreItemGoal = new EmeraldGoal(this, .4D, Ingredient.ofItems(Items.EMERALD, Items.EMERALD_ORE), false);
+
+		goalSelector.add(2, EmeraldBlockGoal);
+		goalSelector.add(2, EmeraldOreItemGoal);
 	}
 }
