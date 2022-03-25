f7526ede4673e1daa6c82247d3ffdbc26bc97764
Arbee4ever <robin.baenecke@mail.de>
2022-03-25 21:43:41 +0100

    ChestBoat test

--- src/main/java/net/arbee/addola/AddolaClient.java
+++ src/main/java/net/arbee/addola/AddolaClient.java
@@ -1,17 +1,38 @@
 package net.arbee.addola;
 
-import net.arbee.addola.entity.renderer.ChestBoatEntityRenderer;
+import net.arbee.addola.entity.vehicle.ChestBoatEntity;
+import net.arbee.addola.network.SpawnChestBoatEntityPacketSender;
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.client.rendereregistry.v1.EntityRendererRegistry;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
 
+import java.util.UUID;
+
+@Environment(EnvType.CLIENT)
 public class AddolaClient implements ClientModInitializer {
 
     @Override
     public void onInitializeClient() {
-
-        EntityRendererRegistry.INSTANCE.register(Addola.CHESTBOAT, (dispatcher, context) -> {
-            return new ChestBoatEntityRenderer(dispatcher);
+        ClientPlayNetworking.registerGlobalReceiver(SpawnChestBoatEntityPacketSender.IDENTIFIER, (client, handler, buf, responseSender) -> {
+            final int entityId = buf.readInt();
+            final UUID uuid = buf.readUuid();
+            final double x = buf.readDouble();
+            final double y = buf.readDouble();
+            final double z = buf.readDouble();
+            final double xVelocity = buf.readDouble();
+            final double yVelocity = buf.readDouble();
+            final double zVelocity = buf.readDouble();
+            final float pitch = buf.readFloat();
+            final float yaw = buf.readFloat();
+            final ChestBoatEntity boat = new ChestBoatEntity(Addola.CHESTBOAT, client.world);
+            boat.setEntityId(entityId);
+            boat.setUuid(uuid);
+            boat.setPos(x, y, z);
+            boat.setVelocity(xVelocity, yVelocity, zVelocity);
+            boat.setYaw(yaw);
+            boat.pitch = pitch;
+            client.execute(() -> client.world.addEntity(entityId, boat));
         });
-
     }
 }
--- src/main/java/net/arbee/addola/entity/renderer/ChestBoatEntityRenderer.java
+++ src/main/java/net/arbee/addola/entity/renderer/ChestBoatEntityRenderer.java
@@ -22,37 +22,38 @@ public class ChestBoatEntityRenderer extends EntityRenderer<ChestBoatEntity> {
         this.shadowRadius = 0.8F;
     }
 
-    public void render(ChestBoatEntity boatEntity, float f, float g, MatrixStack matrixStack, VertexConsumerProvider vertexConsumerProvider, int i) {
+    public void render(ChestBoatEntity chestBoatEntity, float f, float g, MatrixStack matrixStack, VertexConsumerProvider vertexConsumerProvider, int i) {
+        System.out.print("YES");
         matrixStack.push();
         matrixStack.translate(0.0D, 0.375D, 0.0D);
         matrixStack.multiply(Vector3f.POSITIVE_Y.getDegreesQuaternion(180.0F - f));
-        float h = (float)boatEntity.getDamageWobbleTicks() - g;
-        float j = boatEntity.getDamageWobbleStrength() - g;
+        float h = (float)chestBoatEntity.getDamageWobbleTicks() - g;
+        float j = chestBoatEntity.getDamageWobbleStrength() - g;
         if (j < 0.0F) {
             j = 0.0F;
         }
 
         if (h > 0.0F) {
-            matrixStack.multiply(Vector3f.POSITIVE_X.getDegreesQuaternion(MathHelper.sin(h) * h * j / 10.0F * (float)boatEntity.getDamageWobbleSide()));
+            matrixStack.multiply(Vector3f.POSITIVE_X.getDegreesQuaternion(MathHelper.sin(h) * h * j / 10.0F * (float)chestBoatEntity.getDamageWobbleSide()));
         }
 
-        float k = boatEntity.interpolateBubbleWobble(g);
+        float k = chestBoatEntity.interpolateBubbleWobble(g);
         if (!MathHelper.approximatelyEquals(k, 0.0F)) {
-            matrixStack.multiply(new Quaternion(new Vector3f(1.0F, 0.0F, 1.0F), boatEntity.interpolateBubbleWobble(g), true));
+            matrixStack.multiply(new Quaternion(new Vector3f(1.0F, 0.0F, 1.0F), chestBoatEntity.interpolateBubbleWobble(g), true));
         }
 
         matrixStack.scale(-1.0F, -1.0F, 1.0F);
         matrixStack.multiply(Vector3f.POSITIVE_Y.getDegreesQuaternion(90.0F));
-        this.model.setAngles(boatEntity, g, 0.0F, -0.1F, 0.0F, 0.0F);
-        VertexConsumer vertexConsumer = vertexConsumerProvider.getBuffer(this.model.getLayer(this.getTexture(boatEntity)));
+        this.model.setAngles(chestBoatEntity, g, 0.0F, -0.1F, 0.0F, 0.0F);
+        VertexConsumer vertexConsumer = vertexConsumerProvider.getBuffer(this.model.getLayer(this.getTexture(chestBoatEntity)));
         this.model.render(matrixStack, vertexConsumer, i, OverlayTexture.DEFAULT_UV, 1.0F, 1.0F, 1.0F, 1.0F);
-        if (!boatEntity.isSubmergedInWater()) {
+        if (!chestBoatEntity.isSubmergedInWater()) {
             VertexConsumer vertexConsumer2 = vertexConsumerProvider.getBuffer(RenderLayer.getWaterMask());
             this.model.getBottom().render(matrixStack, vertexConsumer2, i, OverlayTexture.DEFAULT_UV);
         }
 
         matrixStack.pop();
-        super.render(boatEntity, f, g, matrixStack, vertexConsumerProvider, i);
+        super.render(chestBoatEntity, f, g, matrixStack, vertexConsumerProvider, i);
     }
 
     @Override
