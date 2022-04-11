7b4c9fffee2aca06482c35d9eef28ab442565bd4
Arbee4ever <robin.baenecke@mail.de>
2022-04-12 00:48:54 +0200

    ChestBoat test

--- src/main/java/net/arbee/addola/Addola.java
+++ src/main/java/net/arbee/addola/Addola.java
@@ -1,17 +1,10 @@
 package net.arbee.addola;
 
-import net.arbee.addola.entity.vehicle.ChestBoatEntity;
-import net.arbee.addola.registries.AddolaEntities;
-import net.arbee.addola.registries.AddolaItems;
 import net.arbee.addola.registries.AddolaCommands;
+import net.arbee.addola.registries.AddolaEntities;
 import net.arbee.addola.registries.AddolaGamerules;
+import net.arbee.addola.registries.AddolaItems;
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.object.builder.v1.entity.FabricEntityTypeBuilder;
-import net.minecraft.entity.EntityDimensions;
-import net.minecraft.entity.EntityType;
-import net.minecraft.entity.SpawnGroup;
-import net.minecraft.util.Identifier;
-import net.minecraft.util.registry.Registry;
 
 public class Addola implements ModInitializer {
     public static final String MOD_NAME = "Addola";
@@ -20,7 +13,7 @@ public class Addola implements ModInitializer {
     public void onInitialize() {
         AddolaGamerules.setupGamerules();
         AddolaCommands.setupCommands();
-        AddolaItems.setupItems();
         AddolaEntities.setupEntities();
+        AddolaItems.setupItems();
     }
 }
--- src/main/java/net/arbee/addola/registries/AddolaItems.java
+++ src/main/java/net/arbee/addola/registries/AddolaItems.java
@@ -1,9 +1,13 @@
 package net.arbee.addola.registries;
 
+import net.arbee.addola.client.render.BoatItemRenderer;
+import net.arbee.addola.client.render.ChestBoatItemRenderer;
 import net.arbee.addola.item.ChestBoatItem;
+import net.fabricmc.fabric.api.client.rendering.v1.BuiltinItemRendererRegistry;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.minecraft.entity.vehicle.BoatEntity;
 import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Items;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
@@ -22,5 +26,20 @@ public class AddolaItems {
         Registry.register(Registry.ITEM, new Identifier("addola", "dark_oak_chestboat"), DARKOAK_CHESTBOAT_ITEM);
         Registry.register(Registry.ITEM, new Identifier("addola", "birch_chestboat"), BIRCH_CHESTBOAT_ITEM);
         Registry.register(Registry.ITEM, new Identifier("addola", "spruce_chestboat"), SPRUCE_CHESTBOAT_ITEM);
+    }
+
+    public static void setupItemRenderers() {
+        BuiltinItemRendererRegistry.INSTANCE.register(Items.ACACIA_BOAT, new BoatItemRenderer());
+        BuiltinItemRendererRegistry.INSTANCE.register(Items.OAK_BOAT, new BoatItemRenderer());
+        BuiltinItemRendererRegistry.INSTANCE.register(Items.BIRCH_BOAT, new BoatItemRenderer());
+        BuiltinItemRendererRegistry.INSTANCE.register(Items.DARK_OAK_BOAT, new BoatItemRenderer());
+        BuiltinItemRendererRegistry.INSTANCE.register(Items.JUNGLE_BOAT, new BoatItemRenderer());
+        BuiltinItemRendererRegistry.INSTANCE.register(Items.SPRUCE_BOAT, new BoatItemRenderer());
+        BuiltinItemRendererRegistry.INSTANCE.register(OAK_CHESTBOAT_ITEM, new ChestBoatItemRenderer());
+        BuiltinItemRendererRegistry.INSTANCE.register(SPRUCE_CHESTBOAT_ITEM, new ChestBoatItemRenderer());
+        BuiltinItemRendererRegistry.INSTANCE.register(BIRCH_CHESTBOAT_ITEM, new ChestBoatItemRenderer());
+        BuiltinItemRendererRegistry.INSTANCE.register(JUNGLE_CHESTBOAT_ITEM, new ChestBoatItemRenderer());
+        BuiltinItemRendererRegistry.INSTANCE.register(ACACIA_CHESTBOAT_ITEM, new ChestBoatItemRenderer());
+        BuiltinItemRendererRegistry.INSTANCE.register(DARKOAK_CHESTBOAT_ITEM, new ChestBoatItemRenderer());
     }
 }
